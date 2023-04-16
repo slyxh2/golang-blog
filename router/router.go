d7b4d360459505b3d95bd3e037448e4f49c7eeb9
slyxh2 <750772121@qq.com>
2023-04-16 19:27:36 -0400

    hot update

--- router/router.go
+++ router/router.go
@@ -1,6 +1,10 @@
 package router
 
 import (
+	"log"
+	"os"
+
+	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -9,5 +13,12 @@ func Init(client *mongo.Client) {
 	router := gin.Default()
 	db := client.Database("blog")
 	HandleUserRouter(router, db)
-	router.Run()
+	// router.Run()
+	err := endless.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Server on 8080 stopped")
+
+	os.Exit(0)
 }
